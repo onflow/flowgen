425bd93bde9f0875c8dba097cfba7bdbe58efa62
Tom Beckenham <34339192+tombeckenham@users.noreply.github.com>
2025-05-18 19:50:37 +1000

    Updated header info

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navigation from "./components/Navigation";
 import { FlowContextProvider } from "./context/flow-context";
 import Header from "./components/header";
 
@@ -18,16 +17,6 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
 	title: "FlowGen",
 	description: "AI-generated pixel art on the Flow blockchain",
-
-	icons: {
-		icon: ["/favicon.ico", "/favicon-96x96.png"],
-		apple: "/apple-touch-icon.png",
-
-		other: {
-			rel: "manifest",
-			url: "/site.webmanifest",
-		},
-	},
 };
 
 export default function RootLayout({
@@ -37,18 +26,6 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			{/*
-			  Next.js will automatically add a <head> tag.
-			  You can add favicon links directly here, or manage them via the `metadata` object above for Next.js 13+.
-			  If you use an online generator and it provides <link> and <meta> tags,
-			  you can paste them here. For example:
-
-			  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-			  <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-			  <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-			  <link rel="manifest" href="/site.webmanifest">
-			  <meta name="theme-color" content="#ffffff">
-			*/}
 			<body
 				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
 			>
